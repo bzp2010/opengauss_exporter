2aca9044b6c807f8a5679018633db213c93fea0b
bzp2010 <bzp2010@apache.org>
2021-09-21 10:30:52 +0800

    fix: build script date

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ VERSION=1.0.0
 GIT_HASH=$(HASH="ref: HEAD"; while [[ $HASH == ref\:* ]]; do HASH="$(cat ".git/$(echo $HASH | cut -d \  -f 2)")"; done; echo ${HASH:0:7})
 DATE=$(date)
 
-GOLDFLAGS="-X github.com/prometheus/common/version.Version=${VERSION} -X github.com/prometheus/common/version.Branch=${GIT_HASH} -X github.com/prometheus/common/version.BuildDate=${DATE}"
+GOLDFLAGS="-X 'github.com/prometheus/common/version.Version=${VERSION}' -X 'github.com/prometheus/common/version.Branch=${GIT_HASH}' -X 'github.com/prometheus/common/version.BuildDate=${DATE}'"
 
 set -x
 
